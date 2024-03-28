4dc4ffde49df5ceef700dba5b1d679447c1ca816
Iajor <rjai.neto@gmail.com>
2024-03-27 21:01:22 -0300

    desafio em java

--- desafio/DesafioJava/Desafio.java
+++ desafio/DesafioJava/Desafio.java
@@ -13,6 +13,68 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Desafio {
+    static void populate(List<List<Integer>> mainList, int sizeOfMainList, int sizeOfInnerList){
+        for (int i = 0; i < sizeOfMainList; i++) {
+            List<Integer> subList = new ArrayList<Integer>();
+            while (subList.size() != sizeOfInnerList) {
+                Random random = new Random();
+                int element = random.nextInt(sizeOfInnerList*10);
+                if (!subList.contains(element)){
+                    subList.add(element);
+                }   
+            }
+            subList.sort(null);
+            mainList.add(subList);
+        }
+    }
+    static void printMainList(List<List<Integer>> mainList){
+        
+        int index = 1;
+        for (List<Integer> subList : mainList) {
+            System.out.println("Exibindo lista " + index);
+            for (int element : subList) {
+                System.out.println(element);
+            }
+            index++;
+        }
+
+    }
+    static void printUnionList(List<List<Integer>> mainList, List<Integer> unionList) {
+        for (List<Integer> subList : mainList) {
+            for (int element : subList) {
+                if (!unionList.contains(element)){
+                    unionList.add(element);
+                }
+            }
+        }
+        unionList.sort(null);;
+        System.out.println("Exbibindo lista união: ");
+        for (int element : unionList) {
+            System.out.println(element);
+        }
+    }
+    static void printUniqueList(List<List<Integer>> mainList, List<Integer> uniqueList){
+        for (List<Integer> subList : mainList) {
+            for (int element : subList) {
+                boolean isUnique = true;
+                for (List<Integer> otherSubList : mainList) {
+                    if (subList != otherSubList && otherSubList.contains(element)){
+                        isUnique = false;
+                        break;
+                    }            
+                }
+                if (isUnique){
+                uniqueList.add(element);
+                }
+            }
+        }
+        uniqueList.sort(null);
+        System.out.println("Exbibindo lista unicos: ");
+        for (int element : uniqueList) {
+            System.out.println(element);
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -22,30 +84,17 @@ public class Desafio {
         int sizeOfInnerList = input.nextInt();
 
         List<List<Integer>> mainList = new ArrayList<List<Integer>>();
-        populate(mainList, sizeOfMainList, sizeOfInnerList):
-        
-        for (int i = 0; i < sizeOfMainList; i++) {
-            List<Integer> subList = new ArrayList<Integer>();
-            while (subList.size() != sizeOfInnerList) {
-                Random random = new Random();
-                int element = random.nextInt(sizeOfInnerList);
-                if (!subList.contains(element)){
-                    subList.add(element);
-                }   
-            }
-            subList.sort(null);
-            mainList.add(subList);
-        }
-        
-        for (List<Integer> subList : mainList) {
-            for (int i = 0; i < mainList.size(); i++) {
-                System.out.println("Exibindo lista "+(i+1));
-            }
-            for (int element : subList) {
-                System.out.println(element);
-            }
-        }
+        populate(mainList, sizeOfMainList, sizeOfInnerList);
+        printMainList(mainList);
+
+        List<Integer> unionList = new ArrayList<Integer>();
+        printUnionList(mainList, unionList);
+
+        List<Integer> uniqueList = new ArrayList<Integer>();
+        printUniqueList(mainList, uniqueList);
+
+
         input.close();
     }
-} 
+}
     
